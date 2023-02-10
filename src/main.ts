8b36ee71069ce5d7df4dc01969f8cabb9f1025ec
yangrenwei <yangrenwei@czltek.com>
2023-02-10 10:13:33 +0800

    vue3—ts-template-全面

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import pinia from '/@/stores/index';
+import App from './App.vue';
+import router from './router';
+import { directive } from '/@/directive/index';
+import other from '/@/utils/other';
+
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import '/@/theme/index.scss';
+import VueGridLayout from 'vue-grid-layout';
+
+const app = createApp(App);
+
+directive(app);
+other.elSvg(app);
+
+app.use(pinia).use(router).use(ElementPlus).use(VueGridLayout).mount('#app');
